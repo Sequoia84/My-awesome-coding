c7f4b38e77a4acd5be328844b83a406af4f361f8
Sequoia84 <82052717+Sequoia84@users.noreply.github.com>
2021-05-07 20:05:15 -0700

    now

--- src/script.js
+++ src/script.js
@@ -17,55 +17,73 @@ function formatDate(date) {
       "Thursday",
       "Friday",
       "Saturday"
-    ];
-    function showWeather(response) {
-      let h1 = document.querySelector("h1");
-      let temperature = Math.round(response.data.main.temp);
-      h1.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
-    }
-  
-    let day = days[dayIndex];
-  
-    return `${day} ${hours}:${minutes}`;
-  }
-  
-  function search(event) {
-    event.preventDefault();
-    let cityElement = document.querySelector("#city");
-    let cityInput = document.querySelector("#city-input");
-    cityElement.innerHTML = cityInput.value;
-  }
-  
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 66;
-  }
-  
-  function convertToCelsius(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = 19;
-  }
-  
-  // Feature #1
-  let dateElement = document.querySelector("#date");
-  let currentTime = new Date();
-  dateElement.innerHTML = formatDate(currentTime);
-  
-  // Feature #2
-  let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", search);
-  
-  // Bonus Feature
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", convertToFahrenheit);
-  
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", convertToCelsius);
-  function searchCity(city) {
-    let apiKey = "16b011e84ccaeeedc0d35d25c7339c80";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
+    
+];
+let day = days[dayIndex];
+
+return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+document.querySelector("#city").innerHTML = response.data.name;
+document.querySelector("#temperature").innerHTML = Math.round(
+  response.data.main.temp
+);
+
+document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+document.querySelector("#wind").innerHTML = Math.round(
+  response.data.wind.speed
+);
+document.querySelector("#description").innerHTML =
+  response.data.weather[0].main;
+}
+
+function searchCity(city) {
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+let city = document.querySelector("#city-input").value;
+searchCity(city);
+}
+
+function searchLocation(position) {
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+  position.coords.latitude
+}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+event.preventDefault();
+navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = 19;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("San Fernando");
